d08397723586b70a089c97621c63b6b07d89e811
jeffman <jefferbrecht@gmail.com>
2016-07-07 21:01:43 -0400

    Added a simple module compiler

---
+++ RopeSnake/Core/Compiler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RopeSnake.Core
+{
+    public sealed class Compiler
+    {
+        private Block _romData;
+        private IAllocator _allocator;
+        private IEnumerable<IModule> _modules;
+
+        public int AllocationAlignment { get; set; } = 1;
+
+        private Compiler() { }
+
+        public static Compiler Create(Block romData, IAllocator allocator, IEnumerable<IModule> modules)
+        {
+            Compiler compiler = new Compiler();
+            compiler._romData = romData;
+            compiler._allocator = allocator;
+            compiler._modules = modules;
+            return compiler;
+        }
+
+        public void Compile()
+        {
+            int alignment = AllocationAlignment;
+            var allocatedPointers = new Dictionary<string, int>();
+
+            foreach (var module in _modules)
+            {
+                var blocks = module.Serialize();
+
+                foreach (var blockPair in blocks)
+                {
+                    int pointer = _allocator.Allocate(blockPair.Value.Size, alignment);
+                    allocatedPointers.Add(blockPair.Key, pointer);
+                }
+
+                var allocatedBlocks = new AllocatedBlockCollection(blocks, allocatedPointers);
+                module.WriteToRom(_romData, allocatedBlocks);
+            }
+        }
+    }
+}

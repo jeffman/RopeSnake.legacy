a034dab4484b5b98591407d11e29bbfdf7c2f4e9
jeffman <jefferbrecht@gmail.com>
2016-09-27 00:19:09 -0400

    Added ArrayLengthAttribute for validation

---
+++ RopeSnake/Core/Validation/ArrayLengthAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace RopeSnake.Core.Validation
+{
+    public class ArrayLengthAttribute : ValidateRuleBaseAttribute
+    {
+        private int[] _lengths;
+
+        public ArrayLengthAttribute(int[] lengths)
+        {
+            if (lengths == null)
+                throw new ArgumentNullException(nameof(lengths));
+
+            _lengths = lengths;
+        }
+
+        protected override bool ValidateInternal(object value, LazyString path, Logger log)
+        {
+            var array = value as Array;
+            if (array == null)
+                throw new Exception("Value must be an array");
+
+            if (array.Rank != _lengths.Length)
+                throw new Exception($"Array has a rank of {array.Rank} but expected {_lengths}");
+
+            for (int i = 0; i < array.Rank; i++)
+            {
+                int length = array.GetLength(i);
+                if (length != _lengths[i])
+                {
+                    return Fail($"Array length (rank {i}) was {length}, but expected {_lengths[i]}", path, log);
+                }
+            }
+
+            return true;
+        }
+    }
+}
